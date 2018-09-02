a0cf041aa59d66a6814459f049ec49ffd266d4ed
Tobe O <thosakwe@gmail.com>
2018-09-02 00:08:23 -0400

    Parse input args

--- src/Binc.Compiler/CompilerOptions.cs
+++ src/Binc.Compiler/CompilerOptions.cs
@@ -5,25 +5,34 @@ namespace Binc.Compiler
 {
     public class CompilerOptions
     {
+        private List<string> mArguments = new List<string>();
+        private OptionSet mOptionSet;
+
         public string InputFilename;
 
         public bool ShowHelp = false;
 
-        List<string> mArguments = new List<string>();
 
         public List<string> Arguments
         {
             get { return mArguments; }
         }
 
+
+        public OptionSet OptionSet
+        {
+            get { return mOptionSet; }
+        }
+
         public static CompilerOptions ParseCommandLineArguments(string[] args)
         {
             var options = new CompilerOptions();
-            var optionSet = new OptionSet() {
-                {"help", v => options.ShowHelp = true}
-            };
+            var optionSet = new OptionSet();
+
+            optionSet.Add("h|help", "Display this help information.", _ => options.ShowHelp = true);
 
             options.mArguments.AddRange(optionSet.Parse(args));
+            options.mOptionSet = optionSet;
             return options;
         }
     }
--- src/Binc/Program.cs
+++ src/Binc/Program.cs
@@ -6,7 +6,16 @@ namespace Binc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var options = Compiler.CompilerOptions.ParseCommandLineArguments(args);
+
+            if (options.ShowHelp) {
+                Console.WriteLine("usage: binc [options...] <input_file>");
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                options.OptionSet.WriteOptionDescriptions(System.Console.Out);
+            } else {
+                Console.WriteLine("TODO: Anything else");
+            }
         }
     }
 }
