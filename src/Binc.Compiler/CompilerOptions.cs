8cdfe816ffee13f14d981dcdd5666af9c7cabc24
Tobe O <thosakwe@gmail.com>
2018-09-02 00:00:35 -0400

    Add basic compiler options

--- src/Binc.Compiler/CompilerOptions.cs
+++ src/Binc.Compiler/CompilerOptions.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using NDesk.Options;
+
 namespace Binc.Compiler
 {
     public class CompilerOptions
@@ -5,5 +8,23 @@ namespace Binc.Compiler
         public string InputFilename;
 
         public bool ShowHelp = false;
+
+        List<string> mArguments = new List<string>();
+
+        public List<string> Arguments
+        {
+            get { return mArguments; }
+        }
+
+        public static CompilerOptions ParseCommandLineArguments(string[] args)
+        {
+            var options = new CompilerOptions();
+            var optionSet = new OptionSet() {
+                {"help", v => options.ShowHelp = true}
+            };
+
+            options.mArguments.AddRange(optionSet.Parse(args));
+            return options;
+        }
     }
 }
